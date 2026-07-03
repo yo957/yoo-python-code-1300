c1ee96c2edb1ff83c7dc5b4fed3a6205b15fe4fd
yo957 <nitis.j@ku.th>
2026-07-03 15:24:47 +0700

    my first commit in class

--- labs/week01-lab/example/syntax_basics.py
+++ labs/week01-lab/example/syntax_basics.py
@@ -77,19 +77,20 @@ print("a =", a, "b =", b, "c =", c)
 # Complete the following assignments:
 
 # String variable containing your favorite color
-favorite_color = 
+favorite_color = "Blue"
 
 # Integer variable with the current year
-current_year = 
+current_year = "2550"
 
 # Float variable with your favorite number (with decimal)
-favorite_number = 
+favorite_number = "8"
 
 # Boolean variable indicating if you like pizza
-likes_pizza = 
+likes_pizza = "No"
 
 # Print all variables with descriptive messages
 print("My favorite color is:", favorite_color)
 print("The current year is:", current_year)
 print("My favorite number is:", favorite_number)
 print("Do I like pizza?", likes_pizza)
+print("hello")
